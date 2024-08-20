a167b0620f27d33d74ad56a5cdd50f97c13642b3
sandeep-kalla <dark1178rider@gmail.com>
2024-08-20 21:24:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Manually split large chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Splits node_modules into a separate chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the warning limit for chunk sizes
+  },
 })
